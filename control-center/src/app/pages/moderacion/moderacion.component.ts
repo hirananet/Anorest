6af436b61238ecc730792d85ffed50a14b451f56
Alexander Eberle <alexander171294@gmail.com>
2021-02-19 14:06:45 -0300

    correcciones

--- control-center/src/app/pages/moderacion/moderacion.component.ts
+++ control-center/src/app/pages/moderacion/moderacion.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModeracionComponent implements OnInit {
 
+  public moderaciones: any[];
+
   constructor() { }
 
   ngOnInit(): void {
